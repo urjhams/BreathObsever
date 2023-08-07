8045b4ef4689637151e43b3978a1c4d0df4c5c16
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2023-08-07 18:50:33 +0200

    apply FFT

--- Sources/BreathObsever/BreathObsever.swift
+++ Sources/BreathObsever/BreathObsever.swift
@@ -11,6 +11,9 @@ public class BreathObsever: ObservableObject {
 
   let session = AVAudioSession()
   
+  /// time of one update cycle
+  let cycle: TimeInterval
+  
   /// timer
   var timer: AnyPublisher<Date, Never>?
   
@@ -46,8 +49,17 @@ public class BreathObsever: ObservableObject {
   // TODO: the model will have a timer(?) to track the density of the peaks in an amount of time
   // -> the breathing pattern is fast or slow -> cognitive load/ calmness level(?)
   
-  public init() {
-    
+  var audioBuffer: [Float] = []
+  let sampleRate = 44100
+  lazy var bufferSize = Int(10 * (1 / cycle)) // 10 seconds
+//  let fftBufferSize = 1024  // size for FFT analysis
+  
+//  let threshold: Float = 0.02
+  
+  var analyzeTimer: Timer?
+  
+  public init(cycle: TimeInterval) {
+    self.cycle = cycle
     // setup audio recorder, if failed, the recorder will be nil
     // try? setupAudioRecorder()
   }
@@ -117,12 +129,10 @@ extension BreathObsever {
     
     let settings: [String: Any] = [
       // 192 kHz is the highest commonly used sample rate
-      AVSampleRateKey:          192000.0,
-      AVEncoderBitRateKey:      192000,
+      AVSampleRateKey:          sampleRate,
       AVFormatIDKey:            Int(kAudioFormatAppleLossless),
       AVNumberOfChannelsKey:    1,
-      AVLinearPCMBitDepthKey:   32,
-      AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue,
+      AVEncoderAudioQualityKey: AVAudioQuality.max.rawValue,
     ]
     
     try recorder = AVAudioRecorder(url: url, settings: settings)
@@ -155,10 +165,24 @@ extension BreathObsever {
     // range from -160 dBFS to 0 dBFS
     let power = recorder.averagePower(forChannel: channel)
     
+    // Convert dB value to linear scale
     digitalPowerLevel = Double(power)
     
-    convertedPowerLevel = convertAudioSignal(power)
+    let convtered = convertAudioSignal(power)
     
+    // this converted power level is used for real time data
+    convertedPowerLevel = convtered
+    
+    // append the data to the buffer to normalizing
+    updateAudioBuffer(with: [Float(convtered)])
+    
+  }
+  
+  private func updateAudioBuffer(with data: [Float]) {
+    audioBuffer.append(contentsOf: data)
+    if audioBuffer.count > bufferSize {
+      audioBuffer.removeFirst(audioBuffer.count - bufferSize)
+    }
   }
   
   /// The peakPower(forChannel:) function in AVFoundation returns the peak power of an
@@ -176,8 +200,67 @@ extension BreathObsever {
 extension BreathObsever {
   public func startTrackAudioSignal() {
     recorder?.record()
+    analyzeTimer = .scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { [weak self] _ in
+      guard self?.isTracking ?? false else {
+        return
+      }
+      self?.performFFTAnalysis()
+    })
   }
   public func stopTrackAudioSignal() {
     recorder?.stop()
+    analyzeTimer?.invalidate()
+    analyzeTimer = nil
   }
+}
+
+import Accelerate
+
+extension BreathObsever {
+  
+  func performFFTAnalysis() {
+    guard audioBuffer.count >= bufferSize else {
+      return
+    }
+        
+    // perform FFT analysis
+    var realPart = audioBuffer
+    var imaginaryPart = [Float](repeating: 0, count: bufferSize)
+    realPart.withUnsafeMutableBufferPointer { realPointee in
+      imaginaryPart.withUnsafeMutableBufferPointer { imaginaryPointee in
+        
+        guard
+          let real = realPointee.baseAddress,
+          let imaginary = imaginaryPointee.baseAddress
+        else {
+          return
+        }
+        
+        var splitComplex = DSPSplitComplex(realp: real, imagp: imaginary)
+        
+        let log2n = vDSP_Length(log2(Float(bufferSize)))
+        guard let setup = vDSP_create_fftsetup(log2n, FFTRadix(kFFTRadix2)) else {
+          return
+        }
+        
+        vDSP_fft_zip(setup, &splitComplex, 1, log2n, FFTDirection(kFFTDirection_Forward))
+        vDSP_destroy_fftsetup(setup)
+        
+        // Calculate magnitude values
+        var magnitude: [Float] = .init(repeating: 0.0, count: bufferSize)
+        vDSP_zvmags(&splitComplex, 1, &magnitude, 1, vDSP_Length(bufferSize))
+        
+        // normalize FFT values
+        let normalizedValues = normalizeFFTValues(magnitude)
+        
+      }
+    }
+  }
+  
+  func normalizeFFTValues(_ values: [Float]) -> [Int] {
+    let max = values.max() ?? 1.0
+    // linear normalization between 0 and 10000
+    return values.map { Int(($0 / max) * 10000) }
+  }
+  
 }
