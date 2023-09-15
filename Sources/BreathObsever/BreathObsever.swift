afb9cdb3ba8559c7def509227ca88edf7fbc93f7
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2023-09-15 21:46:42 +0200

    WIP adding soundAnalysis into BreathObserver, remove BreathClassifier

--- Sources/BreathObsever/BreathObsever.swift
+++ Sources/BreathObsever/BreathObsever.swift
@@ -1,4 +1,5 @@
 import AVFAudio
+import SoundAnalysis
 import Combine
 import Accelerate
 
@@ -9,144 +10,90 @@ public class BreathObsever: ObservableObject {
     case notRecording
     case noTimerAllocated
     case noAvailableInput
+    case noMicrophoneAccess
   }
 
-  let session = AVAudioSession()
+  let audioSession = AVAudioSession.sharedInstance()
   
   private var recorder: AVAudioRecorder?
-    
-  /// time of one update cycle
-  let cycle: TimeInterval
   
-  /// timer
-  var timer: AnyPublisher<Date, Never>?
+  private let analysisQueue = DispatchQueue(label: "com.quan.BreathObserver.AnalysisQueue")
+  
+  private var audioEngine: AVAudioEngine?
+  
+  private var soundAnalysisSubject: PassthroughSubject<SNClassificationResult, Error>?
   
   private var cancellables = Set<AnyCancellable>()
   
-  /// A flag that indicate that currentlly recording
-  @Published
-  public var isTracking = false {
-    didSet {
-      isTracking ? startTrackAudioSignal() : stopTrackAudioSignal()
-    }
-  }
-  
   internal var fftAnalyzer = FFTAnlyzer()
-  
-  /// A flag that indicate if the timer is successfully created
-  @Published
-  public var hasTimer = false
-  
-  /// A flag that indicate if the setupAudioRecorder() has run successfully
-  @Published
-  public var successfullySetupRecord = false
   
   @Published
   public var digitalPowerLevel: Double = 0
   
   @Published
   public var convertedPowerLevel: Int = 0
-  
-  /// State to know is the session successfully set up and available
-  @Published
-  public var sessionAvailable = false
-  
-  public var endTime: TimeInterval
-  
-  private var cycleCounter = 0
-  
-  // TODO: need a model to store the powerlevel array's peaks (up and down)
-  // then use them to indicate cognitive load level
-  
-  let sampleRate = 44100.0
+      
+  public init() {
     
-  public init(cycle: TimeInterval, end: TimeInterval) {
-    self.cycle = cycle
-    self.endTime = end
-    
-    do {
-      try setupAudioRecorder()
-      sessionAvailable = true
-    } catch {
-      sessionAvailable = false
-    }
-
   }
 }
 
-// MARK: - setup
+// MARK: AudioSession
 extension BreathObsever {
-  
-  public func assignTimer(timer: AnyPublisher<Date, Never>) throws {
-    self.timer = timer
-    try setupTimer()
-    hasTimer = true
-  }
-  
-  public func deallocateTimer() {
-    cancellables.removeAll()
-  }
-  
-  private func setupTimer() throws {
-    guard let timer else {
-      throw ObserverError.noTimerAllocated
-    }
-    timer.sink { [weak self] _ in
-      guard let tracking = self?.isTracking, tracking else {
-        return
+  private func ensureMicrophoneAccess() throws {
+    var hasMicrophoneAccess = false
+    switch AVCaptureDevice.authorizationStatus(for: .audio) {
+    case .notDetermined:
+      let sem = DispatchSemaphore(value: 0)
+      AVCaptureDevice.requestAccess(for: .audio) { success in
+        hasMicrophoneAccess = success
+        sem.signal()
       }
-      try? self?.trackAudioSignal()
-    }
-    .store(in: &cancellables)
-  }
-  
-  public func setupAudioRecorder() throws {
-    
-    defer {
-      successfullySetupRecord = true
+      _ = sem.wait(timeout: DispatchTime.distantFuture)
+    case .denied, .restricted:
+      break
+    case .authorized:
+      hasMicrophoneAccess = true
+    @unknown default:
+      fatalError("unknown authorization status for microphone access")
     }
     
-    // record if from phone's mic, playAndRecord if in AirPods
-    //try AVInstance.setCategory(.record)
-    typealias Options = AVAudioSession.CategoryOptions
-    let options: Options = [.allowBluetooth, .allowBluetoothA2DP, .allowAirPlay]
-    try session.setCategory(.playAndRecord,  mode: .measurement, options: [options])
-    
-    guard let availableInputs = session.availableInputs else {
-      throw ObserverError.noAvailableInput
-    }
-        
-    let availableBluetoothLE = availableInputs.first { description in
-      // bluetooth hand free profile, BLE - like AirPods
-      [.bluetoothLE, .bluetoothHFP, .airPlay, .bluetoothA2DP].contains(description.portType)
-    }
-    
-    if let _ = availableBluetoothLE  {
-      try sessionAndRecorderConfig()
+    if !hasMicrophoneAccess {
+      throw ObserverError.noMicrophoneAccess
     }
   }
   
-  private func sessionAndRecorderConfig() throws {
-    try session.setActive(true, options: .notifyOthersOnDeactivation)
-    
-    let filePaths = NSTemporaryDirectory()
-    let url = URL(fileURLWithPath: filePaths).appendingPathComponent("tempRecord")
-    
-    let settings: [String: Any] = [
-      // 192 kHz is the highest commonly used sample rate
-      AVSampleRateKey:          sampleRate,
-      AVFormatIDKey:            Int(kAudioFormatAppleLossless),
-      AVNumberOfChannelsKey:    1,
-      AVEncoderAudioQualityKey: AVAudioQuality.max.rawValue,
-    ]
-    
-    try recorder = AVAudioRecorder(url: url, settings: settings)
-    
-    guard let recorder else {
-      throw ObserverError.recorderNotAllocated
+  private func stopAudioSession() {
+    autoreleasepool { [weak self] in
+      try? self?.audioSession.setActive(false)
     }
-    recorder.prepareToRecord()
-    recorder.isMeteringEnabled = true
+  }
+  
+  private func startAudioSession() throws {
+    stopAudioSession()
+    do {
+      typealias Options = AVAudioSession.CategoryOptions
+      let options: Options = [.allowBluetooth, .allowBluetoothA2DP, .allowAirPlay]
+      try audioSession.setCategory(.record, mode: .measurement, options: options)
+      
+      let allowedPorts: [AVAudioSession.Port] = [
+        .bluetoothLE,
+        .bluetoothHFP,
+        .airPlay,
+        .bluetoothA2DP
+      ]
+      guard
+        let inputs = audioSession.availableInputs,
+        let _ = inputs.first(where: { description in allowedPorts.contains(description.portType) })
+      else {
+        throw ObserverError.noAvailableInput
+      }
+      
+      try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+    } catch {
+      stopAudioSession()
+      throw error
+    }
   }
 }
 
@@ -165,7 +112,6 @@ extension BreathObsever {
     
     recorder.updateMeters()
     
-    cycleCounter += 1
         
     // range from -160 dBFS to 0 dBFS
     let power = recorder.averagePower(forChannel: 0)
@@ -179,15 +125,10 @@ extension BreathObsever {
     
     fftAnalyzer.appendAndAnalyze(
       audioPower: power,
-      time: Int(sampleRate * cycle) // `cycle` seconds at `sampeRate` Hz
+      time: 441000 // `cycle` seconds at `sampeRate` Hz
     )
     
-    guard cycleCounter <= Int(endTime) else {
-      fftAnalyzer.analyzeCurrentDataSet()
-      //TODO: stop the timer, end the session
-      //TODO: need empty the data when start a new tracking
-      return
-    }
+    fftAnalyzer.analyzeCurrentDataSet()
     
     // -- convert to 0-10000 scale and show in real time graph
     // Convert dB value to linear scale
