29dbfd9a5738851e7bb619710538f8eaa2e047a6
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2024-03-06 20:06:24 +0100

    add file for AVCaptureAudioDataOutputSampleBufferDelegate

--- Sources/BreathObsever/BreathObsever.swift
+++ Sources/BreathObsever/BreathObsever.swift
@@ -102,60 +102,6 @@ public class BreathObsever: NSObject, ObservableObject {
   
 }
 
-extension BreathObsever: AVCaptureAudioDataOutputSampleBufferDelegate {
-  public func captureOutput(
-    _ output: AVCaptureOutput,
-    didOutput sampleBuffer: CMSampleBuffer,
-    from connection: AVCaptureConnection
-  ) {
-    var audioBufferList = AudioBufferList()
-    var blockBuffer: CMBlockBuffer?
-    CMSampleBufferGetAudioBufferListWithRetainedBlockBuffer(
-      sampleBuffer,
-      bufferListSizeNeededOut: nil,
-      bufferListOut: &audioBufferList,
-      bufferListSize: MemoryLayout.stride(ofValue: audioBufferList),
-      blockBufferAllocator: nil,
-      blockBufferMemoryAllocator: nil,
-      flags: kCMSampleBufferFlag_AudioBufferList_Assure16ByteAlignment,
-      blockBufferOut: &blockBuffer)
-    
-    guard let data = audioBufferList.mBuffers.mData else {
-      return
-    }
-    
-    /// Because the audio spectrogram code requires exactly `sampleCount` (which the app defines
-    /// as 512) samples, but audio sample buffers from AVFoundation may not always contain exactly
-    /// 512 samples, the app adds the contents of each audio sample buffer to `rawAudioData`.
-    ///
-    /// The following code creates an array from `data` and appends it to  `rawAudioData`:
-    if rawAudioData.count < Self.samples * 2 {
-      let actualSampleCount = CMSampleBufferGetNumSamples(sampleBuffer)
-      let pointer = data.bindMemory(to: Int16.self, capacity: actualSampleCount)
-      let buffer = UnsafeMutableBufferPointer(start: pointer, count: actualSampleCount)
-      
-      rawAudioData.append(contentsOf: buffer)
-    }
-    
-    /// The following code app passes the first `sampleCount`elements of raw audio data to the
-    /// `processData(values:)` function, and removes the first `hopCount` elements from
-    /// `rawAudioData`.
-    ///
-    /// By removing fewer elements than each step processes, the rendered frames of data overlap,
-    /// ensuring no loss of audio data.
-    while rawAudioData.count >= Self.samples {
-      let dataToProcess = Array(rawAudioData[0 ..< Self.samples])
-      rawAudioData.removeFirst(Self.hopCount)
-      processData(values: dataToProcess)
-    }
-    
-    DispatchQueue.main.async { [unowned self] in
-      let amplitude = timeDomainBuffer.reduce(0.0) { max($0, abs($1)) }
-      amplitudeSubject.send(amplitude)
-    }
-  }
-}
-
 extension BreathObsever {
   func processData(values: [Int16]) {
     print(values)
